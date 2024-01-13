a45e59a4ab2a4d732e156cbce62b03513704dcc8
Patrick510 <patrickdedinho321xz@gmail.com>
2024-01-13 16:56:28 -0400

    Tratamento de Excessões

--- src/main/java/com/project/crud/resource/ProgramaResource.java
+++ src/main/java/com/project/crud/resource/ProgramaResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.project.crud.javabeans.Programa;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class ProgramaResource {
 
 	@Autowired
